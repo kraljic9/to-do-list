fb6fc90721fa007de07fc464c21f94b2cfa2e9d1
Pavle Kraljic <kraljicpavle9@gmail.com>
2025-09-27 18:19:50 +0200

    Added a render list function that will add a new list element with a span and a remove event listener

--- index.js
+++ index.js
@@ -3,7 +3,28 @@ let list = document.querySelector(".list");
 let displayInput = document.querySelector(".display-input");
 
 // Task input section
-
+let inputContainer = document.querySelector(".task-input");
 let closeInputContainer = document.querySelector(".close");
 let inputTask = document.querySelector("input");
-let addTask = document.querySelector("task-add");
+let addTask = document.querySelector(".task-add");
+
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+function renderList() {
+  inputTask.innerHTML = "";
+
+  tasks.forEach((task, index) => {
+    let newList = document.createElement("li");
+    let newRemove = document.createElement("span");
+
+    newList.innerHTML = task;
+    newList.classList.add("list-item");
+    newRemove.innerHTML = "X";
+
+    newRemove.addEventListener("click", () => {
+      tasks.splice(index, 1);
+      localStorage.setItem("tasks", JSON.parse("tasks"));
+      renderList();
+    });
+  });
+}
