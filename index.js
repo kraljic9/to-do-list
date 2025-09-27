faf344d739bfe5d78ebbc308c599387f5780a2ea
Pavle Kraljic <kraljicpavle9@gmail.com>
2025-09-27 16:07:25 +0200

    Selected elements in javascript, changed button classes and names

---
+++ index.js
@@ -0,0 +1,9 @@
+// Container section
+let list = document.querySelector(".list");
+let displayInput = document.querySelector(".display-input");
+
+// Task input section
+
+let closeInputContainer = document.querySelector(".close");
+let inputTask = document.querySelector("input");
+let addTask = document.querySelector("task-add");
